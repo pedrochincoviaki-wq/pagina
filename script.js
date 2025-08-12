4810ecb51a55cb8e04fc1fb7e24cb71c762bc019
pedrochincoviaki-wq <pedro.chincoviaki@escola.pr.gov.br>
2025-08-11 22:11:31 -0300

    01

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Obtém o elemento do botão pelo seu ID
+const meuBotao = document.getElementById('meuBotao');
 
+// Obtém o elemento do parágrafo pelo seu ID
+const mensagem = document.getElementById('mensagem');
+
+// Adiciona um "ouvinte" de evento de clique ao botão
+meuBotao.addEventListener('click', function() {
+    // Quando o botão for clicado, muda o texto do parágrafo
+    mensagem.textContent = 'Você clicou no botão! Parabéns por criar sua primeira página!';
+});
